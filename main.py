6070c2f83eb5f177e86eea68caa4e7164ddc90b3
john doe <scottbruceburton@gmail.com>
2020-08-10 20:42:20 -0400

    lietarlly 4 lines

--- main.py
+++ main.py
@@ -1,23 +1,13 @@
 #! python3 
 # make a set of alphabetic directories import string
 
-# import os
 from pathlib import Path
 import string
 
-# currentDir = Path.cwd()
-# print(currentDir)
-
-# make a list of the alphabet
 alphabet_string = string.ascii_uppercase
-# alphabet_string = "ABCDEFG...WXYZ" 
-# alphabet_list = list(alphabet_string)
-# alphabet_list = ['A', 'B' ... 'Y', 'Z']
-# think transitioning from str to list isn't needed
 
 for letter in alphabet_string:
     p = Path("temp/{0}".format(letter))
-    # print(p)
     p.mkdir(parents=True, exist_ok=True)
 
 
